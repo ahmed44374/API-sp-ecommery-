7b591cba8c7b4967746dfc431bf6a626e45f4c31
Ahmed Ehab <sabry44377@gmail.com>
2024-11-20 18:54:42 +0200

    set sorting and pagination and custom api response

--- src/main/java/com/example/specommercy/exception/APIException.java
+++ src/main/java/com/example/specommercy/exception/APIException.java
@@ -6,6 +6,7 @@ public class APIException extends RuntimeException{
     @Serial
     private static final long serialVersionUID = 1L;
     public APIException() {}
+
     public APIException(String message){
         super(message);
     }
